287df75e6f63f5853476d68cc8447e2f63bfaeb6
ahossain2677 <43207698+ahossain2677@users.noreply.github.com>
2018-10-11 08:44:51 -0400

    Update Main.java

--- src/Abdul_Hossain/Main.java
+++ src/Abdul_Hossain/Main.java
@@ -1,23 +1,29 @@
 package Abdul_Hossain;
 
 import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args)
     { System.out.println("What is the correct number?");
         Scanner input = new Scanner(System.in) ;
-            // define the range
-            int max = 100;
-            int min = 1;
-            int range = max - min + 1;
-            // generate random number from 1 to 10
-            int rand = (int) (Math.random()* range) + min;
-            String X =(rand);
 
-        if  (String X =Scanner input
-        System.out.println("Correct");)
-        else (
-        System.out.println("Incorrect");)
+        (int rand){
+        int max = 100  ;
+        int min = 1;
+        int range = max - min + 1;
+        // generate random number from 1 to 10
+        int rand = (int) (Math.random()* range) + min;
+        Integer.toString(rand);}
+
+
+
+
+        if(Scanner input = rand){
+        System.out.println("Correct");
+    }else{
+            System.out.println("Incorrect");}
+
 
     }
-    }
+}

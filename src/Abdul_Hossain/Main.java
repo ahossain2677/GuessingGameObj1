73916f864c74ad77ed4082cce5754612d4fb5881
ahossain2677 <43207698+ahossain2677@users.noreply.github.com>
2018-10-15 20:38:21 -0400

    Update Main.java

--- src/Abdul_Hossain/Main.java
+++ src/Abdul_Hossain/Main.java
@@ -2,26 +2,20 @@ package Abdul_Hossain;
 
 import java.util.Scanner;
 
-public class Main {
+public class Objective1
+{
+  public static void main(String args[])
+  {
+   Scanner keyboard = new Scanner(System.in);
 
-    public static void main(String[] args)
-    { System.out.println("What is the correct number?");
-        Scanner input = new Scanner(System.in) ;
+   int a = 1 + (int) (Math.random() * 99);
+   int guess;
 
-        (int rand){
-        int max = 100  ;
-        int min = 1;
-        int range = max - min + 1;
-        // generate random number from 1 to 10
-        int rand = (int) (Math.random()* range) + min;
-        Integer.toString(rand);}
-
-
-
-
-        if(input = rand){
-        System.out.println("Correct");
-    }else{
-            System.out.println("Incorrect");}
-    }
+   System.out.println("I am thinking of a number from 1 to 100 ... guess what it is ?");
+   if {guess = keyboard.nextInt();
+     System.out.println("Correct");
+   else {
+       System.out.println( "Incorrect");
+   }
+  }
 }
